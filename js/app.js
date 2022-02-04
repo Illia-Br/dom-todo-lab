2588ec9d004eb1ebb5c21f6c08151b0d246736ac
Illia Brytun <elbrytun@gmail.com>
2022-02-03 22:02:58 -0500

    add event listener to button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-console.log("sanity check");
 
+// Cashed element references
+const inputEl = document.getElementById('inp');
+const buttonEl = document.querySelector('#submit-button');
+const ulEl = document.querySelector('ul');
 
+// Event Listeners
+
+buttonEl.addEventListener('click', function(evt) {
+  let newLi = document.createElement('li');
+  newLi.textContent = inputEl.value;
+  inputEl.value = '';
+  ulEl.appendChild(newLi);
+})
+
+console.log(inputEl);
